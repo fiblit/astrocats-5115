99c3be2f9017c7da0921d48f27ce8841f621a992
Alex-Desktop <alex.lamoureaux@gmail.com>
2016-11-03 23:52:57 -0500

    slightly expanded on modularity concept example

--- TakeCare/www/js/modular.js
+++ TakeCare/www/js/modular.js
@@ -6,4 +6,18 @@ function page()
 	tasks.innerHTML = "this is a test";
 	app.appendChild(tasks);
 	tasks.innerHTML = "this is another test";
+	writetasks(tasks);
+}	
+
+function writetasks(elem)
+{
+	elem.innerHTML = "";
+	var table = document.createElement("table");
+	elem.appendChild(table);
+	var tr = document.createElement("tr");
+	tr.innerHTML = "<th>task</th> <th>urgency</th> <th>whatever</th>";
+	table.appendChild(tr);
+	tr = document.createElement("tr");
+	tr.innerHTML = "<td>refill my basketballs</td> <td>very</td> <td>thankyou</td>";
+	table.appendChild(tr);
 }
