2ff7c5b157293f8c515da422f8264229933d8897
Dalton <hildr039@umn.edu>
2016-11-14 17:56:23 -0600

    Update to my mental model

--- TakeCare/www/js/modular.js
+++ TakeCare/www/js/modular.js
@@ -1,104 +1,94 @@
 // Sample/test function for loading a page
-function page1()
-{
-	// First find the main app element
-	var app = document.querySelector(".app");
-	
-	// Now clear the page
-	app.innerHTML = "";
-	
-	// Create paragraph element saying "this is page 1!"
-	var pagenum = document.createElement("p");
-	pagenum.innerHTML = "This is page 1";
-	app.appendChild(pagenum);
-	
-	// Create tasks table
-	var tasks = document.createElement("div");
-	writetasks(tasks);
-	app.appendChild(tasks);
-	
-	// Create buttons div with button linking to page 2
-	var buttons = document.createElement("div");
-	buttons.appendChild(self_button("button1", page2)); 
-	app.appendChild(buttons);
-}	
+function page1() {
+    //create the array to store returned HTMLElements
+    var elems = [];
+
+    // Create paragraph element saying "this is page 1!"
+    var pagenum = document.createElement("p");
+    pagenum.innerHTML = "This is page 1";
+    elems.push(pagenum);
+    
+    // Create tasks table
+    var tasks = document.createElement("div");
+    writetasks(tasks);
+    elems.push(tasks);
+    
+    // Create buttons div with button linking to page 2
+    var buttons = document.createElement("div");
+    buttons.appendChild(self_button("button1", page2)); 
+    elems.push(buttons);
+
+    return elems
+}   
 
 // Sample/test function for loading a different page
-function page2()
-{
-	// First find the main app element
-	var app = document.querySelector(".app");
-	
-	// Now clear the page
-	app.innerHTML = "";
-	
-	// Create paragraph element saying "this is page 2!"
-	var pagenum = document.createElement("p");
-	pagenum.innerHTML = "This is page 2";
-	app.appendChild(pagenum);
-	
-	// Create buttons div with button linking to page 1
-	var buttons = document.createElement("div");
-	buttons.appendChild(self_button("button2", page1)); 
-	buttons.appendChild(self_button("button2", onclick_test));
-	app.appendChild(buttons);
+function page2() {
+    //create the array to store returned HTMLElements
+    var elems = []; 
+    
+    // Create paragraph element saying "this is page 2!"
+    var pagenum = document.createElement("p");
+    pagenum.innerHTML = "This is page 2";
+    elems.push(pagenum);
+    
+    // Create buttons div with button linking to page 1
+    var buttons = document.createElement("div");
+    buttons.appendChild(self_button("button2", page1)); 
+    buttons.appendChild(self_button("button2", onclick_test));
+    elems.push(buttons);
+
+    return elems
 }
 
 // Sample/test function for making a table and appending it to a given element
-function writetasks(elem)
-{
-	var table = document.createElement("table");
-	elem.appendChild(table);
-	var tr = document.createElement("tr");
-	tr.innerHTML = "<th>task</th> <th>urgency</th> <th>whatever</th>";
-	table.appendChild(tr);
-	tr = document.createElement("tr");
-	tr.innerHTML = "<td>refill my basketballs</td> <td>very</td> <td>thankyou</td>";
-	table.appendChild(tr);
-	elem.appendChild(table);
+function writetasks(elem) {
+    var table = document.createElement("table");
+    elem.appendChild(table);
+    var tr = document.createElement("tr");
+    tr.innerHTML = "<th>task</th> <th>urgency</th> <th>whatever</th>";
+    table.appendChild(tr);
+    tr = document.createElement("tr");
+    tr.innerHTML = "<td>refill my basketballs</td> <td>very</td> <td>thankyou</td>";
+    table.appendChild(tr);
+    elem.appendChild(table);
 }
 
-// alink_button
-// Simple function to write a button with an anchor element to link to another page
-// First argument is the text that appears on button
-// Second is url being linked to
-function alink_button(button_text, link_location)
-{
-	var a_elem = document.createElement("a");
-	a_elem.href = link_location;
-	var link_button = document.createElement("input");
-	link_button.type = "button";
-	link_button.value = button_text;
-	a_elem.appendChild(link_button);
-	return a_elem;
-}
-
-// jlink_button
+// link_button
 // Simple function to write a button with an javascript onclick function
 // First argument is the text that appears on button
 // Second is the onclick function (the object, not its name)
-function jlink_button(button_text, link_function)
-{
-	var link_button = document.createElement("input");
-	link_button.type = "button";
-	link_button.value = button_text;
-	link_button.onclick = link_function;
-	return link_button;
+function link_button(button_text, page) {
+    link_button = self_button(button_text, function () {
+        build_page()
+    });
+    return link_button;
+}
+
+function __init() {
+    build_page(page1);
+}
+
+function build_page(page) {
+    var app = document.querySelector(".app");
+    app.innerHTML = "";
+    //app.appendChild(page());//given that a page returns an HTMLElement. 
+    pageElements = page();//given that a page returns an array of HTMLElements.
+    for (int i = 0; i < pageElements.length; i++) {
+        app.appendChild(pageElements[i]);
+    }
 }
 
 // self_button
 // takes button text and a callback function as input
 // creates and returns a button whose onclick value is the callback function
-function self_button(button_text, link_function)
-{
-	var link_button = document.createElement("input");
-	link_button.type = "button";
-	link_button.value = button_text;
-	link_button.onclick = link_function;
-	return link_button;
+function self_button(button_text, callback) {
+    var button = document.createElement("input");
+    button.type = "button";
+    button.value = button_text;
+    button.onclick = callback;
+    return button;
 }
 
-function onclick_test()
-{
-	alert("clicked");
+function onclick_test() {
+    alert("clicked");
 }
