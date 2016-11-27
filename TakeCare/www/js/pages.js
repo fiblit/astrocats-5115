aa05a3c24bac79da7b4dd826c9d52762477dbab7
brint013 <brint013@umn.edu>
2016-11-27 16:27:37 -0600

    Update pages.js

--- TakeCare/www/js/pages.js
+++ TakeCare/www/js/pages.js
@@ -274,11 +274,10 @@ function addtasks() {
         var taskname = text_area.querySelector("#taskname > input").value;
         var date = text_area.querySelector("#date > input").value;
         var time = text_area.querySelector("#time > input").value;
-        if (careteam == null || taskname == null || date == null || time == null || careteam.trim()=="" || taskname.trim()=="" || date.trim()=="" || time.trim()=="") {
-            var p = document.createElement("p");
-            p.innerHTML = "Error: Field is required.";
-            p.id = "error";
-            text_area.appendChild(p);
+        if (careteam == null || careteam.trim()=="") {
+            alert('Error: Field is required.')
+            text_area.querySelector("#careteam > input").focus();
+           
         }
         else {
          build_page(tasks);
