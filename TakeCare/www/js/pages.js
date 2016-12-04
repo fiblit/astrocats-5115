e4c01b8e84c3732f3d2dca89b85433ebf9fda5a0
brint013 <brint013@umn.edu>
2016-12-04 15:00:41 -0600

    Update pages.js

--- TakeCare/www/js/pages.js
+++ TakeCare/www/js/pages.js
@@ -281,12 +281,12 @@ function tasks() {
     for (var team in followedTeams) {
     	var name = document.createElement("p");
     	name.HTML = team;
-    	elems.push(name);
+    	
         for (var task in database['teams'][team]['tasks']) {
 
             followedTasks.push(database['teams'][team]['tasks'][task]);
         }
-    }
+    
 
     followedTasks.map(function( e ) {
         var d = new Date(e['time']);
@@ -294,7 +294,9 @@ function tasks() {
         return e;
     });
     var task_list = data_list(followedTasks);
+    elems.push(name);
     elems.push(task_list);
+    }
 
     //only show "Add a new task" button for CM UI
     var ownedTeams = ufilter(database['teams'] , function(e, name) {
