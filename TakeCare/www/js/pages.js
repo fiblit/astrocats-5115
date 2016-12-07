7093b791ad325236cc9d1fc8fc3f23dfa88c6ffa
Dalton Hildreth <hildr039@umn.edu>
2016-12-06 20:21:40 -0600

    Add team name to update listings

--- TakeCare/www/js/pages.js
+++ TakeCare/www/js/pages.js
@@ -559,8 +559,8 @@ function updates() {
     var followedUpdates = [];
     for (var team in followedTeams) {
         for (var update in database['teams'][team]['updates']) {
-
             followedUpdates.push(database['teams'][team]['updates'][update]);
+            followedUpdates[followedUpdates.length-1]['team'] = team;
         }
     }
     followedUpdates.map(function( e ) {
