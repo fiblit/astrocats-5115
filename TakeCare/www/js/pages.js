2c7e2f3dc5768e5953a0250ed9c8c7695f29ebb8
brint013 <brint013@umn.edu>
2016-12-10 21:09:37 -0600

    Update pages.js

--- TakeCare/www/js/pages.js
+++ TakeCare/www/js/pages.js
@@ -162,7 +162,7 @@ function landing() {
         return e;
     });
    // elems.push(ownedTeams_list);
-   	if (ownedTeams_list == []){
+   	if (ownedTeams_list == null){
    		h.innerHTML = "You currently do not help manage any Care Teams"
    	}
    	else{
